c3d64148df21ad16ade2b36e5466bd6b0e84d5d0
Randall Duval <Randall Duval@C1246895-WIN64-AIR>
2017-03-28 20:23:37 -0700

    add scroll bar notes

--- Tkinter_Tutorial/src/scrollbar_example.py
+++ Tkinter_Tutorial/src/scrollbar_example.py
@@ -6,14 +6,15 @@ Created on Mar 26, 2017
 from tkinter import *
 
 root = Tk()
-scrollbar = Scrollbar(root)
-scrollbar.pack( side = RIGHT, fill=Y )
+scrollbar = Scrollbar(root, bg = 'red')
+scrollbar.pack( side = RIGHT, fill=Y)
 
-mylist = Listbox(root, yscrollcommand = scrollbar.set )
+mylist = Listbox(root, yscrollcommand = scrollbar.set,bd = 6, bg = 'green' )
+
 for line in range(100):
    mylist.insert(END, "This is line number " + str(line))
 
 mylist.pack( side = LEFT, fill = BOTH )
-scrollbar.config( command = mylist.yview )
+scrollbar.config( command = mylist.yview)
 
 mainloop()
