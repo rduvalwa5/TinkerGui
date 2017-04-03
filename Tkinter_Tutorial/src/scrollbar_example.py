69f78378317d39e6fd5e6e5979ebd6a9471adbeb
rduvalwa5 <rduvalwa5@hotmail.com>
2017-04-02 21:37:52 -0700

    tkinter scroll bar horizonal

--- Tkinter_Tutorial/src/scrollbar_example.py
+++ Tkinter_Tutorial/src/scrollbar_example.py
@@ -6,15 +6,17 @@ Created on Mar 26, 2017
 from tkinter import *
 
 root = Tk()
-scrollbar = Scrollbar(root, bg = 'red')
+scrollbar = Scrollbar(root)
+scrollbarh = Scrollbar(root,orient=HORIZONTAL,width=10)
 scrollbar.pack( side = RIGHT, fill=Y)
-
-mylist = Listbox(root, yscrollcommand = scrollbar.set,bd = 6, bg = 'green' )
+scrollbarh.pack(side = BOTTOM, fill=X)
+#self.xscrollbar = Scrollbar(self.root, orient=HORIZONTAL, command=self.canvas.xview)
+mylist = Listbox(root, yscrollcommand = scrollbar.set, xscrollcommand = scrollbarh.set,bd = 6, bg = 'green' ,width = 100)
 
 for line in range(100):
    mylist.insert(END, "This is line number " + str(line))
 
 mylist.pack( side = LEFT, fill = BOTH )
 scrollbar.config( command = mylist.yview)
-
+scrollbarh.config(command = mylist.xview)
 mainloop()
